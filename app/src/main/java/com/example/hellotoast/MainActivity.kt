202574a0381780dda7635d01fd9a70543ab9a422
Raflhy Nur Ramadhan <raflhy.pragma@gmail.com>
2025-10-01 13:06:15 +0700

    second commit

---
+++ app/src/main/java/com/example/hellotoast/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.hellotoast
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.net.Uri
+
+
+class MainActivity : AppCompatActivity() {
+    private var mCount = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val mShowCount = findViewById<TextView>(R.id.show_count)
+        val buttonCountUp = findViewById<Button>(R.id.button_count)
+        val buttonToast = findViewById<Button>(R.id.button_toast)
+        val buttonSwitchPage = findViewById<Button>(R.id.button_switchpage) // <-- Switch Page
+        val buttonBrowser = findViewById<Button>(R.id.button_browser)  //<-- Browser
+
+        //Tombol Count
+        buttonCountUp.setOnClickListener {
+            mCount++
+            mShowCount.text = mCount.toString()
+        }
+
+        //Tombol Toast
+        buttonToast.setOnClickListener {
+            val tulisan = mShowCount.text.toString()
+            Toast.makeText(this, "Angka yang dimunculkan $tulisan", Toast.LENGTH_LONG).show()
+        }
+
+        //Tombol SwitchPage
+        buttonSwitchPage.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+
+        //Tombol Browser
+        buttonBrowser.setOnClickListener(View.OnClickListener {
+            val intentbrowse = Intent(Intent.ACTION_VIEW)
+            intentbrowse.setData(Uri.parse("https://www.google.com/"))
+            startActivity(intentbrowse)
+        })
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
